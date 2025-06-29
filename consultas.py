cbff10b05d604a6270a1e20db9a76e32ae2626ac
JOSUEMC-088 <franciscojosue088@gmail.com>
2025-06-28 21:37:02 -0600

    Update consultas.py

--- consultas.py
+++ consultas.py
@@ -1,14 +1,14 @@
 import flet as ft
 import modelo as md
-import main as ma
+import importlib
 
 def main(page: ft.Page):
 
     def regresar(e: ft.ControlEvent):
         page.clean()
+        ma = importlib.import_module("main")
         ma.main(page)
 
-    # Configurar página
     page.title = "Consultas"
     page.window.width = 800
     page.window.height = 600
@@ -22,10 +22,8 @@ def main(page: ft.Page):
         center_title=True,
     )
 
-    # Botón regresar
     btn_regresar = ft.ElevatedButton(text="Regresar", icon="arrow_back", on_click=regresar)
 
-    # Encabezado de la tabla
     encabezado = [
         ft.DataColumn(ft.Text("Descripción", width=200)),
         ft.DataColumn(ft.Text("Presentación", width=200)),
@@ -34,24 +32,20 @@ def main(page: ft.Page):
         ft.DataColumn(ft.Text("Sustancia activa", width=200))
     ]
 
-    # Cuerpo de la tabla
     filas = []
     medicinas = md.Medicamento.select()
     for med in medicinas:
-        celda1 = ft.DataCell(ft.Text(med.descripcion, weight="bold"))
-        celda2 = ft.DataCell(ft.Text(med.presentacion))
-        celda3 = ft.DataCell(ft.Text(med.clasificacion, italic=True))
-        celda4 = ft.DataCell(ft.Text(med.nivel_atencion))
-        celda5 = ft.DataCell(ft.Text(med.descripcion, color="pink"))  # Verifica si querías `med.sustancia_activa` aquí
-        fila = ft.DataRow([celda1, celda2, celda3, celda4, celda5])
+        fila = ft.DataRow([
+            ft.DataCell(ft.Text(med.descripcion, weight="bold")),
+            ft.DataCell(ft.Text(med.presentacion)),
+            ft.DataCell(ft.Text(med.clasificacion, italic=True)),
+            ft.DataCell(ft.Text(med.nivel_atencion)),
+            ft.DataCell(ft.Text(med.descripcion, color="pink"))  # ¿Querías med.sustancia_activa?
+        ])
         filas.append(fila)
 
-    tbl_medicamentos = ft.DataTable(
-        columns=encabezado,
-        rows=filas
-    )
+    tbl_medicamentos = ft.DataTable(columns=encabezado, rows=filas)
 
-    # Agregar componentes a la página
     page.add(btn_regresar, tbl_medicamentos)
     page.update()
 
